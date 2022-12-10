91acdf59fb515ac18d26770117bed482bd0f2248
Grace Meredith <64381571+meredith-grace@users.noreply.github.com>
2022-12-10 12:00:00 -0800

    Delete config directory

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  get "contact", to: "pages#contact"
-  get "about", to: "pages#about"
-  
-  resources :books
-  root 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
